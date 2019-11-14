fdf97334492e28033ef90b4fd2575ec0212e2d47
mlazzell17@gmail.com <MicheSi>
2019-11-14 09:06:54 -0700

    completed second challenge

--- index.js
+++ index.js
@@ -77,11 +77,24 @@ toString() {
 
 class Car {
   constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
     this.tank = 0;
     this.odometer = 0;
   }
   fill(gallons) {
     this.tank += gallons;
+  }
+  drive(distance) {
+    let maxDistance = this.tank * this.milesPerGallon;
+    if (distance < maxDistance) {
+      this.odometer += distance;
+      this.tank -= distance /this.milesPerGallon;
+    } else if (distance >= maxDistance) {
+      this.odometer += maxDistance;
+      this.tank -= maxDistance / this.milesPerGallon;
+      return `I ran out of fuel at ${this.odometer} miles!`
+    }
   }
 }
 
